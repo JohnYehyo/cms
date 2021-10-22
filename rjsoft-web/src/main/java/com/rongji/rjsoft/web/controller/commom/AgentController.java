2f00059664ce884385a640e90096dd3e025ef9ff
JohnYehyo <836524969@qq.com>
2021-10-22 09:16:05 +0800

    feat: 探活

--- rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/commom/AgentController.java
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/commom/AgentController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "agent")
 @AllArgsConstructor
-public class AgentControlle {
+public class AgentController {
 
     private final ISysDictDataService sysDictDataService;
 
