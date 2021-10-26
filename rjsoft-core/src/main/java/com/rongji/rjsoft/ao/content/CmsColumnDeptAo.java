69f8dcc6fc89900450108c83e2683efbffa254bd
JohnYehyo <836524969@qq.com>
2021-10-26 15:02:53 +0800

    refactor: 站点栏目关系重构

---
+++ rjsoft-core/src/main/java/com/rongji/rjsoft/ao/content/CmsColumnDeptAo.java
@@ -0,0 +1,31 @@
+package com.rongji.rjsoft.ao.content;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @description: 栏目部门关系参数
+ * @author: JohnYehyo
+ * @create: 2021-10-26 14:58:27
+ */
+@Data
+@ApiModel(value = "栏目部门关系入参")
+public class CmsColumnDeptAo {
+
+    /**
+     * 栏目id
+     */
+    @ApiModelProperty(value = "栏目id")
+    @NotNull(message = "栏目不能为空")
+    private Long columnId;
+
+    /**
+     * 部门id
+     */
+    @ApiModelProperty(value = "部门id集合")
+    @NotNull(message = "部门不能为空")
+    private Long[] deptId;
+}
