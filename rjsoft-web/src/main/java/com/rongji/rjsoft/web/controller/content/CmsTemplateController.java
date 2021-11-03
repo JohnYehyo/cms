c1ac0cb860f09b6272bfcb820559568e59971305
JohnYehyo <836524969@qq.com>
2021-11-03 14:02:48 +0800

    style: 删除无用导入

--- rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/content/CmsTemplateController.java
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/content/CmsTemplateController.java
@@ -14,7 +14,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
